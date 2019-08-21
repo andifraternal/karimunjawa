acbef361ff107209c0df54762f883c1e0b127b3e
IST-PC-26 <IST-PC-26@ad.hsinni.com>
2019-08-21 15:57:53 +0700

    21082019 4 sore

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'golekiDewe4y0' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', '127.0.0.1' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
