0871942cf2261fdcc35a98e135fb21f0b2600847
IST-PC-26 <IST-PC-26@ad.hsinni.com>
2019-08-22 16:53:07 +0700

    22agustus-sore

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'pesonakarimunjawa' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'golekiDewe4y0' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
